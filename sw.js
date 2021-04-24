3f7d48436b3da997e6b0faec5c51572af7178c96
SSZX866 <2386316625@qq.com>
2021-04-24 21:01:23 +0800

    update

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210422.1205';
+const cacheName = 'chirpy-20210424.2058';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
